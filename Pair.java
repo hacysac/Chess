5c85f3652ad66eadadc810ed54936389c42ccaac
Hayley Chechel <hayley.chechel@gmail.com>
2026-01-19 09:18:45 -0500

    updated pair file

---
+++ Pair.java
@@ -0,0 +1,16 @@
+public class Pair<U, V> {
+
+    private U first;
+    private V second;
+
+    public Pair(U first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+    public U getFirst() {
+        return first;
+    }
+    public V getSecond() {
+        return second;
+    }
+}
